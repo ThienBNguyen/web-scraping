4bcf87e98340d4e62a747c5914de394886cf464f
ThienBNguyen <thien.nguyen30191@gmail.com>
2023-06-25 23:02:43 -0500

    learn puppeteer

--- index.js
+++ index.js
@@ -1,104 +1,105 @@
-const cors = require('cors')
-const cheerio = require('cheerio')
-// const getUrls = require('get-urls')
-const fs = require('fs/promises')
-const puppeteer = require('puppeteer')
-const url = 'https://www.ooshirts.com/designapp/'
-const ooshirts = async () => {
-    
-    const browser = await puppeteer.launch({headless: true})
-    const page = await browser.newPage()
-    await page.goto(url, { timeout: 60000 })
-    //  await page.waitForTimeout(2000)
-    // await page.screenshot({ path: 'screenshot.png' });
-  const imageUrl = await page.$eval('img', (img) => img.src);
-  console.log(imageUrl);
-  const imageBuffer = await page.goto(imageUrl);
-  console.log(imageBuffer);
-    fs.writeFile('image.jpg', imageBuffer);
-       await browser.close()
-}
-// ooshirts()
-(async () => {
+// const puppeteer = require('puppeteer');
+// const fs = require('fs');
+// const axios = require('axios');
+// async function downloadImage(backgroundImageUrl, fileName) {
+//   const response = await axios.get(backgroundImageUrl, { responseType: 'arraybuffer' });
+
+//   fs.writeFileSync(fileName, Buffer.from(response.data, 'binary'));
+// }
+
+// async function writeFileFromBackgroundImageUrl() {
+//   const browser = await puppeteer.launch();
+//   const page = await browser.newPage();
+
+//   const url = 'https://www.ooshirts.com/designapp/'; // Replace with the actual URL
+//   const selector = '#canvas'; // Replace with the actual CSS selector for the element
+
+//   await page.goto(url, { timeout: 60000 });
+//   await page.waitForSelector(selector);
+
+//   const backgroundImageUrl = await page.evaluate(() => {
+//     const sectionElement = document.querySelector('section#canvas');
+//     const style = getComputedStyle(sectionElement);
+//     const backgroundImage = style.backgroundImage;
+//     const url = backgroundImage.slice(5, -2);
+//     return url;
+//   });
+
+//   await browser.close();
+
+//   await downloadImage(backgroundImageUrl, 'test.jpg');
+// }
+
+
+
+
+
+
+// async function writeFileFromBackgroundImageUrl() {
+//   const browser = await puppeteer.launch();
+//   const page = await browser.newPage();
+
+//   const url = 'https://www.ooshirts.com/designapp/'; // Replace with the actual URL
+//   const selector = '#canvas'; // Replace with the actual CSS selector for the element
+
+//   try {
+//     await page.goto(url, { timeout: 60000 });
+//     await page.waitForSelector(selector);
+
+//     const backgroundImageUrl = await page.evaluate(() => {
+//       const sectionElement = document.querySelector('section#canvas');
+//       const style = getComputedStyle(sectionElement);
+//       const backgroundImage = style.backgroundImage;
+//       const url = backgroundImage.slice(5, -2);
+//       return url;
+//     });
+
+//     await page.goto(backgroundImageUrl, { timeout: 80000 });
+//     const imageBuffer = await page.screenshot();
+
+//     fs.writeFile('test.jpg', imageBuffer, (error) => {
+//       if (error) {
+//         console.error('Error writing image:', error);
+//       } else {
+//         console.log('Image downloaded successfully!');
+//       }
+//     });
+//   } catch (error) {
+//     console.error('Error:', error);
+//   } finally {
+//     await browser.close();
+//   }
+// }
+
+// writeFileFromBackgroundImageUrl();
+const puppeteer = require('puppeteer');
+const axios = require('axios');
+const fs = require('fs');
+
+async function downloadImagesFromWebsite(url) {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-
-  let counter = 0;
-  page.on('response', async (response) => {
-    const matches = /.*\.(jpg|png|svg|gif)$/.exec(response.url());
-    // console.log(matches, 'matches');
-    if (matches && (matches.length === 2)) {
-             const extension = matches[1];
-      const buffer = await response.buffer();
-    //   fs.writeFile(`images/image-${counter}.${extension}`, buffer, 'base64');
-      fs.writeFile(`images/${matches.input.split('/')[matches.input.split('/').length - 1]}`, buffer, 'base64');
-
-      counter += 1;
-    //   console.log(counter);
-    }
-  });
 
   await page.goto(url);
-
-  await browser.close();
-});
-(async () => {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-
-const selector = '#canvas'; // Replace with the actual CSS selector for the element
-const url = 'https://www.ooshirts.com/designapp/'
- await page.goto(url);
-   
-  await page.waitForSelector(selector);
-//   await page.timeout(2000)
-    const backgroundImageUrl = await page.evaluate(() => {
-  const element = document.querySelector(selector); // Replace with the actual CSS selector for the element
-        console.log(getComputedStyle(element));
-  return getComputedStyle(element).backgroundImage.slice(4, -1).replace(/['"]/g, '');
-// return null
-});
-// backgroundImageUrl()
-await page.goto(backgroundImageUrl);
-
-const imageBuffer = await page.screenshot();
-// console.log(imageBuffer);
-console.log();
-  await browser.close();
-});
-async function writeFileFromBackgroundImageUrl() {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-
-  const url = 'https://www.ooshirts.com/designapp/'; // Replace with the actual URL
-  const selector = '#canvas'; // Replace with the actual CSS selector for the element
-
-  await page.goto(url, { timeout: 60000 })
-  await page.waitForSelector(selector);
-//   const bodyHandle = await page.$(selector);
-//   const html = await page.evaluate(body => body.innerHTML, bodyHandle);
-// await bodyHandle.dispose();
-  const backgroundImageUrl = await page.evaluate(() => {
-    const sectionElement = document.querySelector('section#canvas');
-    const style = getComputedStyle(sectionElement);
-    const backgroundImage = style.backgroundImage;
-    const url = backgroundImage.slice(5, -2);
-    return url;
+//get the list of url in the array that using img tag
+  const imageUrls = await page.evaluate(() => {
+    const imgElements = document.querySelectorAll('img');
+    const urls = Array.from(imgElements).map((img) => img.src);
+    return urls;
   });
-console.log(backgroundImageUrl);
-//   const backgroundImageUrl = await page.evaluate((selector) => {
-//     const element = document.querySelector(selector);
-//     return getComputedStyle(element).backgroundImage.slice(4, -1).replace(/['"]/g, '');
-//   }, );
-
-//   await page.goto(backgroundImageUrl);
-
-//   const imageBuffer = await page.screenshot();
-
-  // Write the image buffer to a file using fs.writeFileSync()
-  // ...
+// loop over the array urls and download the file 
+  for (let i = 0; i < imageUrls.length; i++) {
+    const imageUrl = imageUrls[i];
+    console.log(imageUrl);
+    const response = await axios.get(imageUrl, { responseType: 'arraybuffer' });
+    console.log(response);
+    const fileName = `image${i}.jpg`; // You can modify the file naming as needed
+    // fs.writeFileSync(fileName, Buffer.from(response.data, 'binary'));
+    console.log(`Image ${i + 1} downloaded: ${fileName}`);
+  }
 
   await browser.close();
 }
 
-writeFileFromBackgroundImageUrl();
+const websiteUrl = 'https://www.ooshirts.com/designapp/'; // Replace with the website URL you want to download images from
+downloadImagesFromWebsite(websiteUrl);
---
+++ index2.js
@@ -0,0 +1,95 @@
+const cors = require('cors')
+const cheerio = require('cheerio')
+// const getUrls = require('get-urls')
+const fs = require('fs/promises')
+const puppeteer = require('puppeteer')
+const url = 'https://www.ooshirts.com/designapp/'
+const ooshirts = async () => {
+    
+    const browser = await puppeteer.launch({headless: true})
+    const page = await browser.newPage()
+    await page.goto(url, { timeout: 60000 })
+    //  await page.waitForTimeout(2000)
+    // await page.screenshot({ path: 'screenshot.png' });
+  const imageUrl = await page.$eval('img', (img) => img.src);
+  console.log(imageUrl);
+  const imageBuffer = await page.goto(imageUrl);
+  console.log(imageBuffer);
+    fs.writeFile('image.jpg', imageBuffer);
+       await browser.close()
+}
+// ooshirts()
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+
+  let counter = 0;
+  page.on('response', async (response) => {
+    const matches = /.*\.(jpg|png|svg|gif)$/.exec(response.url());
+    // console.log(matches, 'matches');
+    if (matches && (matches.length === 2)) {
+             const extension = matches[1];
+      const buffer = await response.buffer();
+    //   fs.writeFile(`images/image-${counter}.${extension}`, buffer, 'base64');
+      fs.writeFile(`images/${matches.input.split('/')[matches.input.split('/').length - 1]}`, buffer, 'base64');
+
+      counter += 1;
+    //   console.log(counter);
+    }
+  });
+
+  await page.goto(url);
+
+  await browser.close();
+});
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+
+const selector = '#canvas'; // Replace with the actual CSS selector for the element
+const url = 'https://www.ooshirts.com/designapp/'
+ await page.goto(url);
+   
+  await page.waitForSelector(selector);
+//   await page.timeout(2000)
+    const backgroundImageUrl = await page.evaluate(() => {
+  const element = document.querySelector(selector); // Replace with the actual CSS selector for the element
+        console.log(getComputedStyle(element));
+  return getComputedStyle(element).backgroundImage.slice(4, -1).replace(/['"]/g, '');
+// return null
+});
+// backgroundImageUrl()
+await page.goto(backgroundImageUrl);
+
+const imageBuffer = await page.screenshot();
+// console.log(imageBuffer);
+console.log();
+  await browser.close();
+});
+async function writeFileFromBackgroundImageUrl() {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+
+  const url = 'https://www.ooshirts.com/designapp/'; // Replace with the actual URL
+  const selector = '#canvas'; // Replace with the actual CSS selector for the element
+
+  await page.goto(url, { timeout: 60000 })
+  await page.waitForSelector(selector);
+//   const bodyHandle = await page.$(selector);
+//   const html = await page.evaluate(body => body.innerHTML, bodyHandle);
+// await bodyHandle.dispose();
+  const backgroundImageUrl = await page.evaluate(() => {
+    const sectionElement = document.querySelector('section#canvas');
+    const style = getComputedStyle(sectionElement);
+    const backgroundImage = style.backgroundImage;
+    const url = backgroundImage.slice(5, -2);
+    return url;
+  });
+  await page.goto(backgroundImageUrl);
+
+  const imageBuffer = await page.screenshot();
+    fs.writeFile('test.jpg', imageBuffer);
+  await browser.close();
+}
+
+writeFileFromBackgroundImageUrl();
